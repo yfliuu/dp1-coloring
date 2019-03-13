1c806e93ccf658a1ba4d9820cea58264b3a349fd
Yifan Liu <yfliu12061060@outlook.com>
2019-03-13 11:41:21 -0500

    AG

---
+++ lib.py
@@ -0,0 +1,20 @@
+import math
+
+
+def choose_prime(delta):
+    # Choose the first prime number between [delta, 2*delta]
+    for n in range(delta, 2 * delta + 1):
+        if is_prime(n):
+            return n
+
+
+def is_prime(n):
+    if n == 2:
+        return True
+    elif n == 1:
+        return False
+
+    for i in range(2, min(n, int(math.sqrt(n)) + 1)):
+        if i % n == 0:
+            return True
+    return False
