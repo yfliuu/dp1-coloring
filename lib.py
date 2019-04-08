c3af639191f9cf42a981ba131e6e762dd9ae3695
Yifan Liu <yfliu12061060@outlook.com>
2019-04-08 10:06:05 -0500

    probably bugs

--- lib.py
+++ lib.py
@@ -1,4 +1,5 @@
 import math
+import networkx as nx
 
 
 def choose_prime(delta):
@@ -14,12 +15,38 @@ def is_prime(n):
     elif n == 1:
         return False
 
-    for i in range(2, min(n, int(math.sqrt(n)) + 1)):
-        if i % n == 0:
-            return True
-    return False
+    for i in range(2, min(n, math.ceil(math.sqrt(n)) + 1)):
+        if n % i == 0:
+            return False
+    return True
 
 
 # Trivially coloring all nodes with a different color.
 def trivial_coloring(G):
     return {i: i for i in range(len(G.nodes()))}
+
+
+def calc_delta(G):
+    return max([d for _, d in G.degree])
+
+
+def gen_random_graph(n=100, radius=0.125):
+    return nx.random_geometric_graph(n, radius)
+
+
+def draw_graph(G):
+    nx.draw(G, pos=nx.spring_layout(G))
+
+
+# def ksquare_coloring(G):
+#     delta = calc_delta(G)
+#     colors = {i: False for i in range(delta * delta)}
+#     current_color = 0
+#     color_mapping = {i: None for i in range(len(G.nodes()))}
+
+#     for nid, deg in sorted(G.degree.items(), key=lambda x: x: [1]):
+#         for neighbor_of_nid in G.neighbors(nid):
+#             color_mapping[neighbor_of_nid] = current_color
+#             colors[current_color] = True
+#             current_color += 1
+#         
--- vis.py
+++ vis.py
@@ -1,11 +1,6 @@
 import plotly
 import plotly.graph_objs as go
-
-import networkx as nx
-
-
-def gen_random_graph(n=100, radius=0.125):
-    return nx.random_geometric_graph(n, radius)
+import lib
 
 
 def edge_trace(G):
@@ -18,7 +13,7 @@ def edge_trace(G):
     return go.Scatter(x=xs, y=ys, line=dict(width=0.5,color='#888'), hoverinfo='none', mode='lines')
 
 
-def node_trace(G, color_mapping):
+def node_trace(G, color_mapping, node_text):
     # color scale options
     # 'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
     # 'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
@@ -34,7 +29,7 @@ def node_trace(G, color_mapping):
 
         node_color = color_mapping[node]
         colors.append(node_color)
-        hover_text.append('Color assigned: ' + str(node_color))
+        hover_text.append('Color assigned: {}, {}'.format(color_mapping[node], node_text[node]))
 
     return go.Scatter(x=xs, y=ys, text=hover_text, 
         mode='markers', hoverinfo='text',
@@ -53,8 +48,8 @@ def node_trace(G, color_mapping):
             line=dict(width=2)))
 
 
-def fig(G, color_mapping):
-    delta = max([d for _, d in G.degree])
+def fig(G, color_mapping, node_text):
+    delta = lib.calc_delta(G)
     layout = go.Layout(title='Graph coloring', titlefont=dict(size=16), showlegend=False,
                        hovermode='closest', margin=dict(b=20, l=5, r=5, t=40),
                        annotations=[dict(
@@ -63,14 +58,16 @@ def fig(G, color_mapping):
                            x=0.005, y=-0.002)],
                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
-    return go.Figure(data=[edge_trace(G), node_trace(G, color_mapping)], layout=layout)
+    return go.Figure(data=[edge_trace(G), node_trace(G, color_mapping, node_text)], layout=layout)
 
 
-def plot(G, color_mapping):
-    plotly.offline.plot(fig(G, color_mapping), filename='gcoloring.html')
+def plot(G, color_mapping=None, filename='gcoloring.html', node_text={}):
+    if color_mapping is None:
+        color_mapping = lib.trivial_coloring(G)
+    plotly.offline.plot(fig(G, color_mapping, node_text=node_text), filename=filename)
 
 
 if __name__ == '__main__':
-    G = gen_random_graph()
+    G = lib.gen_random_graph()
     color_mapping = {i: i for i in range(len(G.nodes()))}
     plot(G, color_mapping)
